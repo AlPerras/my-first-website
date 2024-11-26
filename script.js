88c96ee284d7382caece83a81deeb3378cea0a3d
Alexandre Perras <alexperras74@gmail.com>
2024-11-26 20:53:18 +0800

    Add JavaScript interactivity to website

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Welcome to my first interactive website!");
+
+const button = document.getElementById("greet-btn");
+
+button.addEventListener("click", () => {
+    alert("Hello, Alexandre! You're doing great!");
+});
